52cae8494f1f9735fff37d7a54f9f1c6914d9135
wangbolin <wangbolin@instreet.cn>
2018-08-30 15:00:12 +0800

    add excercise

--- excersise/hello.py
+++ excersise/hello.py
@@ -8,4 +8,25 @@ def testfun(a, b):
 
 a = testfun(2, 3)
 
-print(a,uuid.uuid4())
+# print(a,uuid.uuid4())
+
+
+tuple1=(1,2,3,4,5)
+
+# print(type (tuple1))
+
+
+list1= ['Google', 'Taobao', 'Runoob', 'Baidu',"Baidu"]
+
+tuple2=tuple(list1)
+
+# print(tuple2)
+
+
+param={'a','b','c','d'}
+
+# print(param)
+
+
+set_a=set('abcd')
+print(set_a)
--- excersise/readfile.py
+++ excersise/readfile.py
@@ -1,7 +1,34 @@
-fo = open('前端面试题.docx', 'r', encoding='gbk', errors='ignore')
+# fo = open('前端面试题.docx', 'r', encoding='gbk', errors='ignore')
+#
+# print(fo.name)
+#
+# line = fo.read()
+# print("docx contnet %s" % line)
+#
+# fo.close()
 
-print(fo.name)
 
-line = fo.read()
-print("docx contnet %s" % line)
-fo.close()
+
+
+# with open('123.txt','r') as fo:
+#
+#     # for line in fo.readlines():
+#     #     print(line)
+#     con=fo.read(100)
+#     while len(con):
+#         print(con)
+#         con=fo.read(100)
+
+
+# with open('123.txt','r') as file:
+#     print(file.readline())
+#     # print(file.seek(0))
+#     print(file.readline())
+
+
+fp=open("test.txt",'w')
+
+
+with open('123.txt','r') as file:
+    for line in file.readlines():
+        fp.write(line)
